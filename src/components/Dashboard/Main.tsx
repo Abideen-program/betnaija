551b4e05cdea8bf1a882e1feef9929fd04f590cf
Abideen Olafimihan <olafimihana@gmail.com>
2023-09-25 11:29:23 +0100

    dashboard grant table

--- src/components/Dashboard/Main.tsx
+++ src/components/Dashboard/Main.tsx
@@ -1,27 +1,40 @@
 import DataCard from "./DataCard";
+import Table from "./Table";
 
 const Main = () => {
   return (
     <div className="bg-[#0D141D] text-white p-5 md:p-10 h-max">
-      <h1 className="text-2xl font-medium">Dashboard</h1>
+      <h1 className="text-2xl font-medium mt-14">Dashboard</h1>
 
       <div className="my-7 grid md:grid-cols-2 gap-6">
         <DataCard
           title={"Total Grants Received"}
-          amount={'0.0'}
+          amount={"0.0"}
           label={"Grant"}
         />
         <DataCard
           title={"Total Grant Applications"}
-          amount={'0.0'}
+          amount={"0.0"}
           label={"Applications"}
         />
-        <DataCard title={"Total Active Grants"} amount={'0.0'} label={"Grant"} />
+        <DataCard
+          title={"Total Active Grants"}
+          amount={"0.0"}
+          label={"Grant"}
+        />
         <DataCard
           title={"Total Declined Applications"}
-          amount={'0.0'}
+          amount={"0.0"}
           label={"Applications"}
         />
+      </div>
+
+      <h1 className="text-2xl font-medium mt-14">Grant Status</h1>
+      <div className="my-7 grid md:grid-cols-2 gap-6">
+        <Table />
+        <Table />
+        <Table />
+        <Table />
       </div>
     </div>
   );
---
+++ src/utils/Grant.ts
@@ -0,0 +1,91 @@
+export const activeGrant = [
+  {
+    id: 1,
+    title: "Education",
+    amount: 5000,
+    status: "active",
+  },
+
+  {
+    id: 2,
+    title: "Health",
+    amount: 10000,
+    status: "active",
+  },
+
+  {
+    id: 3,
+    title: "sport",
+    amount: 15000,
+    status: "active",
+  },
+];
+
+export const draftGrant = [
+  {
+    id: 1,
+    title: "Health",
+    amount: 1000,
+    status: "draft",
+  },
+
+  {
+    id: 2,
+    title: "Education",
+    amount: 5000,
+    status: "draft",
+  },
+
+  {
+    id: 3,
+    title: "sport",
+    amount: 4000,
+    status: "draft",
+  },
+];
+
+export const closedGrant = [
+  {
+    id: 1,
+    title: "Health",
+    amount: 1000,
+    status: "disbursed",
+  },
+
+  {
+    id: 2,
+    title: "Community",
+    amount: 50000,
+    status: "disbursed",
+  },
+
+  {
+    id: 3,
+    title: "sport",
+    amount: 41000,
+    status: "disbursed",
+  },
+];
+
+export const declinedGrant = [
+  {
+    id: 1,
+    title: "Community",
+    amount: 2000,
+    status: "declined",
+  },
+
+  {
+    id: 2,
+    title: "Health",
+    amount: 3000,
+    status: "declined",
+  },
+
+  {
+    id: 3,
+    title: "Education",
+    amount: 4000,
+    status: "declined",
+  },
+];
