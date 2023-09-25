884e8e73b4b2dbc88ea98f3cdd77ceb79e194247
Abideen Olafimihan <olafimihana@gmail.com>
2023-09-25 18:45:25 +0100

    created the header dropdown

---
+++ src/components/Header/Dropdown.tsx
@@ -0,0 +1,36 @@
+import { AiOutlineUser } from "react-icons/ai";
+import { GoSignOut } from "react-icons/go";
+import { MdContrast } from "react-icons/md";
+
+const Dropdown = () => {
+  return (
+    <div className="border border-[#182536] fixed right-10 top-[66px] rounded-md text-white w-[300px] z-10 flex flex-col">
+      <div className="flex items-center gap-3 cursor-pointer bg-[#101924] p-5 border-b border-[#8092AB]">
+        <div className="bg-[#9D72FF] h-[35px] w-[35px] flex items-center justify-center rounded-full">
+          T
+        </div>
+        <div className="hidden md:flex flex-col">
+          <p className="text-sm font-bold">TEST ACCOUNT</p>
+          <p className="text-xs text-[#6B7993] flex items-center gap-2">
+            abideen@gmail.com
+          </p>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-3 p-5 bg-[#18212D] text-[#6B7993] hover:text-[#229853] cursor-pointer">
+        <AiOutlineUser className="text-xl" />
+        <span className="text-xs font-medium">View Profile</span>
+      </div>
+      <div className="flex items-center gap-3 p-5 bg-[#18212D] text-[#6B7993] hover:text-[#229853] cursor-pointer">
+        <MdContrast className="text-xl" />
+        <span className="text-xs font-medium">Light Mode</span>
+      </div>
+      <div className="flex items-center gap-3 p-5 bg-[#18212D] text-[#6B7993] hover:text-[#229853] cursor-pointer border-t border-[#8092AB]">
+        <GoSignOut className="text-xl" />
+        <span className="text-xs font-medium">Sign Out</span>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,32 +4,36 @@ import { AiOutlineUser } from "react-icons/ai";
 import { MdArrowDropDown } from "react-icons/md";
 import { FaBars } from "react-icons/fa";
 import { SidebarContext } from "../Context/SidebarContext";
+import Dropdown from "./Dropdown";
 
 const Header = () => {
   const { showSidebar, setShowSidebar } = useContext(SidebarContext);
   return (
-    <div className="bg-[#101924] h-[70px] text-white flex items-center justify-between px-5 md:px-10 border-b border border-[#182536] fixed w-full">
-      <div className="flex items-center justify-between">
-        <div
-          onClick={() => setShowSidebar(!showSidebar)}
-          className="h-[40px] w-[40px] flex items-center justify-center rounded-full hover:bg-[#070A0F] cursor-pointer"
-        >
-          <FaBars className="text-[#6B7993] text-2xl" />
+    <div className="relative">
+      <div className="bg-[#101924] h-[70px] text-white flex items-center justify-between px-5 md:px-10 border-b border border-[#182536] fixed w-full">
+        <div className="flex items-center justify-between">
+          <div
+            onClick={() => setShowSidebar(!showSidebar)}
+            className="h-[40px] w-[40px] flex items-center justify-center rounded-full hover:bg-[#070A0F] cursor-pointer"
+          >
+            <FaBars className="text-[#6B7993] text-2xl" />
+          </div>
+          <img src={AuthLogo} alt="logo" className="w-[100px]" />
         </div>
-        <img src={AuthLogo} alt="logo" className="w-[100px]" />
-      </div>
 
-      <div className="flex items-center gap-3 cursor-pointer">
-        <div className="bg-[#9D72FF] h-[35px] w-[35px] flex items-center justify-center rounded-full">
-          <AiOutlineUser />
-        </div>
-        <div className="hidden md:flex flex-col">
-          <p className="text-xs text-[#136648]">AGENT</p>
-          <p className="text-sm flex items-center gap-2">
-            Abiola Adebiyi <MdArrowDropDown />
-          </p>
+        <div className="flex items-center gap-3 cursor-pointer">
+          <div className="bg-[#9D72FF] h-[35px] w-[35px] flex items-center justify-center rounded-full">
+            <AiOutlineUser />
+          </div>
+          <div className="hidden md:flex flex-col">
+            <p className="text-xs text-[#136648]">AGENT</p>
+            <p className="text-sm flex items-center gap-2">
+              Abiola Adebiyi <MdArrowDropDown />
+            </p>
+          </div>
         </div>
       </div>
+      <Dropdown />
     </div>
   );
 };
